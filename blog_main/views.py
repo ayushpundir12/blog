0a34741f03b69717e7c2bd2725393e199bd479e0
ayush <ayushp198925@gmail.com>
2026-01-26 21:57:52 +0530

    search feature

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from blogs.models import Category,blog
+from assignment.models import About
 
 
 def home(request):
     categories= Category.objects.all()
     featured_blogs= blog.objects.filter(is_featured=True,status='Published').order_by('updated_at')
     posts= blog.objects.filter(status='Published',is_featured=False).order_by('-created_at')
+    
+
+    try:
+        about=About.objects.get()
+    except :
+        about=None
+
 
     contex={'categories': categories, 
             'featured_blogs': featured_blogs,
-             'posts': posts}
+             'posts': posts,
+             'about': about,}
     return render(request,'home.html',contex)
--- blogs/context_processors.py
+++ blogs/context_processors.py
@@ -1,6 +1,10 @@
 from .models import Category
-
+from assignment.models import SocialLink
 
 def get_categories(request):
     categories = Category.objects.all()
     return dict(categories=categories)
+
+def social_links(request):
+    sociallink=SocialLink.objects.all()
+    return dict(sociallink=sociallink)
